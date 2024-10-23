ce97baae5d00f5655ef7553482c2e3e630074ecc
Tomáš Vojíř <vojirtom@fel.cvut.cz>
2024-10-23 15:29:32 +0200

    added IDD datasets

--- benchmark/datasets/__init__.py
+++ benchmark/datasets/__init__.py
@@ -1,4 +1,4 @@
 from .dataset_registry import DatasetRegistry
 
 # Populate the registry
-from .tracks import DatasetAnomalyTrack, DatasetObstacleTrack, DatasetSmallObstacle
+from .tracks import DatasetIDDAnomalyTrack, DatasetIDDObstacleTrack
