c88e9c9dad85851db4957f5b0e980ecfa2c28af2
Tomáš Vojíř <vojirtom@fel.cvut.cz>
2024-10-23 15:33:25 +0200

    results loader method interfacce + save also class prediction from the method output

--- benchmark/example_inference_with_metrics.py
+++ benchmark/example_inference_with_metrics.py
@@ -1,40 +1,58 @@
+import os
 import numpy as np
 from tqdm import tqdm
 import cv2 as cv
 from evaluation import Evaluation
+from types import SimpleNamespace
 
 def method_dummy(image, **_):
-	""" Very naive method: return color saturation """
-	image_hsv = cv.cvtColor(image, cv.COLOR_RGB2HSV_FULL)
-	anomaly_p = image_hsv[:, :, 1].astype(np.float32) * (1./255.)
-	return anomaly_p
+    """ Very naive method: return color saturation """
+    image_hsv = cv.cvtColor(image, cv.COLOR_RGB2HSV_FULL)
+    anomaly_p = image_hsv[:, :, 1].astype(np.float32) * (1./255.)
+    return anomaly_p
 
+def method_result_loader_PixOOD(method_name, dset_name, fid, results_root_dir):
+    # EXAMPLE: 'fid': 'validation_1'
+    # load corresponding saved results
+    result_file = os.path.join(results_root_dir, method_name, fid + ".npz")
+    if not os.path.isfile(result_file):
+        raise FileNotFoundError(f"File not found: {result_file}")
+    # result file should containt two fields
+    results = np.load(result_file)
+    return SimpleNamespace(anomaly_scores = results["anomaly_scores"], 
+                           pred_id_labels = results["pred_id_labels"]) 
 
 def main():
-	ev = Evaluation(
-		method_name = 'Dummy', 
-		dataset_name = 'ObstacleTrack-validation',
-		# dataset_name = 'AnomalyTrack-test',
-	)
+    method_name = "PixOOD_cs_RO"
+    dataset_name = 'IDDObstacleTrack-static'
+    # dataset_name = 'IDDAnomalyTrack-all'
 
-	for frame in tqdm(ev.get_frames()):
-		# run method here
-		result = method_dummy(frame.image)
-		# provide the output for saving
-		ev.save_output(frame, result)
+    ev = Evaluation(
+        method_name = method_name, 
+        dataset_name = dataset_name,
+    )
 
-	# wait for the background threads which are saving
-	ev.wait_to_finish_saving()
+    for frame in tqdm(ev.get_frames()):
+        # run method here
+        # result = method_dummy(frame.image)
+        result = method_result_loader_PixOOD(method_name, dataset_name, frame.fid, results_root_dir="./_results")
+        # provide the output for saving
+        ev.save_output(frame, result)
 
-	ev.calculate_metric_from_saved_outputs(
-		'PixBinaryClass',
-		frame_vis=True,
-	)
+    # wait for the background threads which are saving
+    ev.wait_to_finish_saving()
 
-	ev.calculate_metric_from_saved_outputs(
-		'SegEval-ObstacleTrack',
-		frame_vis=True,
-	)
+    print("Calculating pixel-level metrics")
+    ev.calculate_metric_from_saved_outputs(
+        'PixBinaryClass',
+        frame_vis=True,
+    )
+
+    print("Calculating instance-level metrics")
+    ev.calculate_metric_from_saved_outputs(
+        'SegEval-ObstacleTrack',
+        frame_vis=True,
+    )
 
 if __name__ == '__main__':
-	main()
+    main()
