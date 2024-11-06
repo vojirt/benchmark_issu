4f2bbe768a7a8b97c496cafa2762bff9e77f3771
Tomáš Vojíř <vojirtom@fel.cvut.cz>
2024-11-06 11:03:42 +0100

    Removed special dataset for openset evaluation, the annotation is created and passed in regular dataloader

--- benchmark/datasets/__init__.py
+++ benchmark/datasets/__init__.py
@@ -2,4 +2,3 @@ from .dataset_registry import DatasetRegistry
 
 # Populate the registry
 from .tracks import DatasetIDDAnomalyTrack, DatasetIDDObstacleTrack
-from .tracks_full_taxonomy import DatasetIDDFullAnomalyTrack
